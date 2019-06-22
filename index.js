5a4f01d5c26c1641b7547123d56a100d136ab930
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-22 08:44:59 -0700

    more modularization

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 'use strict';
 
-const fs = require('fs');
 const Bitmap = require('./lib/bitmap');
-
-// ------------------ GET TO WORK ------------------- //
-
-function transformWithCallbacks(operation) {
-
-  fs.readFile(file, (err, buffer) => {
-
-    if (err) {
-      throw err;
-    }
-
-    bitmap.parse(buffer);
-    bitmap.transform(operation);
-
-    // Note that this has to be nested!
-    // Also, it uses the bitmap's instance properties for the name and thew new buffer
-    fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Bitmap Transformed: ${bitmap.newFile}`);
-    });
-
-  });
-}
+const transWith = require('./lib/transWith');
 
 // TODO: Explain how this works (in your README)
 const [file, operation] = process.argv.slice(2);
 
 let bitmap = new Bitmap(file);
 
-transformWithCallbacks(operation);
+transWith.callback(file, bitmap, operation);
 
